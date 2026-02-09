89d971da81239dd1852296116fbbf6387b9b1219
krithika1627 <krithika1627@gmail.com>
2026-02-09 17:04:55 +0530

    Updated homepage with landing page

--- frontend/src/app/profile/page.js
+++ frontend/src/app/profile/page.js
@@ -1,10 +1,11 @@
 "use client"
 import { useUser } from '@clerk/nextjs'
 import React from 'react'
+import { redirect } from "next/navigation";
 
 function page() {
     const {user}=useUser();
-    if(!user) return <p>Loading...</p>;
+    if(!user) redirect("/sign-in");
   return (
     <div>
         <h1>My Profile</h1>
